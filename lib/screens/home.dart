1af8e8bf6785566d177575bc5274d34302a4826d
syipmong <syipmong5@gmail.com>
2024-02-08 20:16:12 +0100

    added the floating action button

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:todo_app/screens/todoscreen.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -13,13 +13,22 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Todo List',
-            style: TextStyle(color: Colors.white, fontSize: 24),
-            textAlign: TextAlign.center),
+        title: const Text(
+          'Todo List',
+          style: TextStyle(color: Colors.white, fontSize: 24),
+          textAlign: TextAlign.center,
+        ),
         centerTitle: true,
         backgroundColor: Colors.blue[900],
       ),
       body: const ToDoScreen(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // functionality to add new task
+        },
+        backgroundColor: Colors.blue[900],
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
